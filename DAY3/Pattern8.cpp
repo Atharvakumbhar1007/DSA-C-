d581fc8d05bafc7e3c052817e28946575ffc4f03
Atharva <atharvakumbhar631@gmail.com>
2026-04-14 19:18:17 +0530

    Patterns

--- DAY3/Pattern8.cpp
+++ DAY3/Pattern8.cpp
@@ -1,25 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void print1(int n){ int space = 2*(n-1);
-	for(int i=1; i<=n; i++){
-		for(int j = 1; j<=i; j++){
-			cout << j;
-			start = 1 - start;
-		}
-		//Space
-		for(int j = 1; j <= space; j++)
-		cout << " ";
+void print1(int n){ 
+    int space = 2 * (n - 1);
 
-		// Number
-		for (int j = i; j >= 1; j --){
-			cout << j;
-		}
-		cout << endl;
-		space -= 2;
-	}
-    
+    for(int i = 1; i <= n; i++){
+        
+        // Numbers (increasing)
+        for(int j = 1; j <= i; j++){
+            cout << j;
+        }
+        
+        // Spaces
+        for(int j = 1; j <= space; j++){
+            cout << " ";
+        }
+
+        // Numbers (decreasing)
+        for (int j = i; j >= 1; j--){
+            cout << j;
+        }
+
+        cout << endl;
+        space -= 2;
+    }
 }
+
 int main (){
     int t;
     cin >> t;
