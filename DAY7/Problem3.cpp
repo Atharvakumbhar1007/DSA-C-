1595014008cf253fb35cbe52a8065ee96a192b04
Atharva <atharvakumbhar631@gmail.com>
2026-04-19 11:31:45 +0530

    problems

---
+++ DAY7/Problem3.cpp
@@ -0,0 +1,13 @@
+//left rotate the array by one place
+
+#include <bits/atdc++.h>
+using namespace std;
+
+rotaterarray(vector<int> & arr, int n){
+int temp = arr[0];
+for (int i = 1; i <n ;i++){
+	arr[i - 1] = arr [i];
+}
+arr[n - 1] = temp;
+return arr;
+}
