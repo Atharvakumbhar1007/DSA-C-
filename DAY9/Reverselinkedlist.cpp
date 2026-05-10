5428eebddb3e3691776fae5231f6c53fe90c072c
Atharva <atharvakumbhar631@gmail.com>
2026-05-10 23:53:09 +0530

    problems

---
+++ DAY9/Reverselinkedlist.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int val) {
+        data = val;
+        next = NULL;
+    }
+};
+
+Node* reverseList(Node* head) {
+    Node* prev = NULL;
+    Node* curr = head;
+
+    while(curr != NULL) {
+        Node* nextNode = curr->next;
+
+        curr->next = prev;
+        prev = curr;
+        curr = nextNode;
+    }
+
+    return prev;
+}
+
+void printList(Node* head) {
+    while(head != NULL) {
+        cout << head->data << " ";
+        head = head->next;
+    }
+}
+
+int main() {
+    Node* head = new Node(1);
+    head->next = new Node(2);
+    head->next->next = new Node(3);
+
+    head = reverseList(head);
+
+    printList(head);
+}
